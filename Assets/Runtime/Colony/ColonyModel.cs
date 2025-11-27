6a829edb23236bad0f7d1740eef365ca12837613
MiramiKoi <153764172+MiramiKoi@users.noreply.github.com>
2025-11-27 19:31:52 +0300

    add: models and descriptions from Colony, Resources and Orders

---
+++ Assets/Runtime/Colony/ColonyModel.cs
@@ -0,0 +1,12 @@
+using Runtime.Colony.Orders;
+
+namespace Runtime.Colony
+{
+    public class ColonyModel
+    {
+        public ColonyDescription Description;
+        public ColonyPopulationModel Population;
+        public ColonyBuildingsModel Buildings;
+        public ColonyOrdersPoolModel OrdersPoolModel;
+    }
+}
---
+++ Assets/Runtime/Colony/ColonyPopulationModel.cs
@@ -0,0 +1,7 @@
+namespace Runtime.Colony
+{
+    public class ColonyPopulationModel
+    {
+        /*public List<Citizen> Citizens;*/
+    }
+}
