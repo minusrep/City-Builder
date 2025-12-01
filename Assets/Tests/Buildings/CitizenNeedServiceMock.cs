df7b442d74f81b3e3e8c44f0a75446850349968d
Valyutik <sergei.lind@tutamail.com>
2025-12-01 16:14:23 +0300

    add: tests for building service needs

---
+++ Assets/Tests/Buildings/CitizenNeedServiceMock.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using Runtime.Colony.Citizens;
+
+namespace Tests.Buildings
+{
+    public sealed class CitizenNeedServiceMock : ICitizenNeedService
+    {
+        public readonly List<(int id, string resource)> Calls = new();
+
+        public void RestoreNeed(int citizenId, string resourceKey)
+        {
+            Calls.Add((citizenId, resourceKey));
+        }
+    }
+}
