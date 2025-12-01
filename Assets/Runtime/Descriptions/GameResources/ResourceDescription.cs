4f6a2ef3468b8a25b42a37bb4bc898d6a9a2dc55
Valyutik <sergei.lind@tutamail.com>
2025-12-01 16:39:18 +0300

    refactor: remove view data from description

--- Assets/Runtime/Descriptions/GameResources/ResourceDescription.cs
+++ Assets/Runtime/Descriptions/GameResources/ResourceDescription.cs
@@ -3,7 +3,6 @@ namespace Runtime.Descriptions.GameResources
     public class ResourceDescription
     {
         public string Type;
-        public string Name;
         public float ReductionTime;
         public float ReductionAmount;
     }
