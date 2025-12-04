43dde23190756416728dc70272638da5d296b988
Valyutik <sergei.lind@tutamail.com>
2025-12-04 20:37:12 +0300

    refactor: add initialization of description collections and description factories

--- Assets/Runtime/Descriptions/Descriptions.cs
+++ Assets/Runtime/Descriptions/Descriptions.cs
@@ -1,13 +1,32 @@
-﻿using Runtime.Colony.Citizens;
+﻿using Runtime.Descriptions.GameResources;
 using Runtime.Descriptions.Buildings;
-using Runtime.Descriptions.GameResources;
+using System.Collections.Generic;
+using Runtime.Colony.Citizens;
 
 namespace Runtime.Descriptions
 {
     public sealed class Descriptions
     {
         public BuildingsDescriptionCollection BuildingDescriptionCollection { get; }
+        
         public ResourceDescriptionCollection ResourceDescriptionCollection { get; }
-        public CitizensDescription citizensDescription { get; }
+        
+        public CitizensDescription CitizensDescription { get; }
+        
+        private DescriptionFactory Factory { get; }
+
+        public Descriptions(Dictionary<string, object> data)
+        {
+            Factory = new DescriptionFactory();
+            Factory.Register<ProductionBuildingDescription>("production");
+            Factory.Register<ServiceBuildingDescription>("service");
+            Factory.Register<DecorBuildingDescription>("decor");
+            Factory.Register<StorageBuildingDescription>("storage");
+            
+            
+            BuildingDescriptionCollection = new BuildingsDescriptionCollection((Dictionary<string, object>)data["buildings"], Factory);
+            ResourceDescriptionCollection = new ResourceDescriptionCollection((Dictionary<string, object>)data["resources"]);
+            CitizensDescription = new CitizensDescription();
+        }
     }
 }
