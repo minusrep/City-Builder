755a3cabfe03f74e157c527186b4849f54a0ec87
Valyutik <sergei.lind@tutamail.com>
2025-12-04 18:57:21 +0300

    add: resource factory

---
+++ Assets/Runtime/Colony/GameResources/ResourceFactory.cs
@@ -0,0 +1,19 @@
+﻿using Runtime.Descriptions.GameResources;
+
+namespace Runtime.Colony.GameResources
+{
+    public sealed class ResourceFactory : IResourceFactory
+    {
+        private readonly ResourceDescriptionCollection _collection;
+
+        public ResourceFactory(ResourceDescriptionCollection collection)
+        {
+            _collection = collection;
+        }
+
+        public ResourceModel Create(string id)
+        {
+            return new ResourceModel(_collection.Descriptions[id]);
+        }
+    }
+}
