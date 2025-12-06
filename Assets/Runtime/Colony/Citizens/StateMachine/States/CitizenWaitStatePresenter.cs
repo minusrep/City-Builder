e80c74216b1ee1af775fdb7edaba5be66353ec5b
Maxim S. <its3zin.main@gmail.com>
2025-12-07 00:15:11 +0300

    rmv: old citizen state machine

--- Assets/Runtime/Colony/Citizens/StateMachine/States/CitizenWaitStatePresenter.cs
+++
@@ -1,9 +0,0 @@
-namespace Runtime.Colony.Citizens.StateMachine.States
-{
-    public class CitizenWaitStatePresenter : CitizenStatePresenter
-    {
-        public override void Update(CitizenStateMachineView view)
-        {
-        }
-    }
-}
