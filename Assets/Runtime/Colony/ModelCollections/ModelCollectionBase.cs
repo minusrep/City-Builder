34771207824591e8294bc417b6797f24d0837fc6
Valyutik <sergei.lind@tutamail.com>
2025-12-06 16:38:12 +0300

    refactor: add a generic key and remove serialization

--- Assets/Runtime/Colony/ModelCollections/ModelCollectionBase.cs
+++ Assets/Runtime/Colony/ModelCollections/ModelCollectionBase.cs
@@ -1,66 +1,31 @@
 ﻿using System.Collections.Generic;
 using System;
-using Runtime.Utilities;
 
 namespace Runtime.Colony.ModelCollections
 {
-    public abstract class ModelCollectionBase<T> : IModelCollection<T> where T : ISerializeModel
+    public abstract class ModelCollectionBase<TKey, TValue> : IModelCollection<TValue>
     {
         public event Action<TValue> OnAdded;
         public event Action<TValue> OnRemoved;
 
-        protected Dictionary<int, T> Models { get; private set; } = new();
-        public int Index { get; protected set; }
+        protected Dictionary<TKey, TValue> Models { get; } = new();
         
-        public void DeleteModel(int id)
+        public void DeleteModel(TKey id)
         {
             var model = Models[id];
             Models.Remove(id);
             OnRemoved?.Invoke(model);
         }
 
-        public T FindModel(int id)
+        public TValue FindModel(TKey id)
         {
             return Models[id];
         }
+
+        protected abstract TValue CreateModelFromData(TKey id, Dictionary<string, object> data);
         
-        public Dictionary<string, object> Serialize()
+        protected void InvokeOnCreateModel(TValue model)
         {
-            var data = new Dictionary<string, object>
-            {
-                { "index", Index }
-            };
-
-            var models = new Dictionary<string, object>();
-            
-            foreach (var model in Models)
-            {
-                models.Add(model.Key.ToString(), model.Value.Serialize());
-            }
-
-            data.Set("models", models);
-            return data;
-        }
-
-        public void Deserialize(Dictionary<string, object> data)
-        {
-            Index = data.GetInt("index");
-            Models = new Dictionary<int, T>();
-
-            var models = data.GetDict("models");
-            foreach (var pair in models)
-            {
-                var modelData = (Dictionary<string, object>)pair.Value;
-                var model = CreateModelFromData(Convert.ToInt32(pair.Key), modelData);
-                Models.Add(Convert.ToInt32(pair.Key), model);
-            }
-        }
-
-        protected abstract T CreateModelFromData(int id, Dictionary<string, object> data);
-        
-        protected void InvokeOnCreateModel(T model)
-        {
-            OnCreateModel?.Invoke(model);
             OnAdded?.Invoke(model);
         }
     }
