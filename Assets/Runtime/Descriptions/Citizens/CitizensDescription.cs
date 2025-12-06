783decbe9152521c12fa12f8a8acac0a56fbabc8
Valyutik <sergei.lind@tutamail.com>
2025-12-06 16:30:45 +0300

    add: deserialization of citizens description

--- Assets/Runtime/Descriptions/Citizens/CitizensDescription.cs
+++ Assets/Runtime/Descriptions/Citizens/CitizensDescription.cs
@@ -1,13 +1,18 @@
-using System;
 using System.Collections.Generic;
+using Runtime.Utilities;
 
-namespace Runtime.Colony.Citizens
+namespace Runtime.Descriptions.Citizens
 {
-    [Serializable]
     public class CitizensDescription
     {
-        public List<string> Names;
+        public List<string> Names { get; }
 
-        public float StartMoveSpeed;
+        public float StartMoveSpeed { get; }
+
+        public CitizensDescription(Dictionary<string, object> data)
+        {
+            Names = data.GetList<string>("names");
+            StartMoveSpeed = data.GetFloat("start_move_speed");
+        }
     }
 }
