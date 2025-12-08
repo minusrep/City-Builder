6c9b866c5ae0e8cee83602f5fa2429c21fb10c78
Maxim S. <its3zin.main@gmail.com>
2025-12-08 19:52:15 +0300

    chg: move descriptions of states machines for folders

--- Assets/Runtime/Descriptions/PointOfInterestDescriptionCollection.cs
+++ Assets/Runtime/Descriptions/PointOfInterestDescriptionCollection.cs
@@ -1,6 +1,6 @@
 using System;
 using System.Collections.Generic;
-using Runtime.Colony.ModelCollections;
+using Runtime.ModelCollections;
 using UnityEngine;
 
 namespace Runtime.StateMachine.Descriptions
