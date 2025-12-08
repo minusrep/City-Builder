fc1762a95c831e36795dc2a88677049d48003630
Valyutik <sergei.lind@tutamail.com>
2025-12-08 20:36:26 +0300

    add: description of the view of the buildings and their collection

---
+++ Assets/Runtime/ViewDescriptions/Buildings/BuildingViewDescriptionCollection.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Runtime.ViewDescriptions.Buildings
+{
+    [CreateAssetMenu(fileName = "BuildingViewDescriptionCollection", menuName = "Buildings/ViewDescriptionCollection")]
+    public sealed class BuildingViewDescriptionCollection : ScriptableObject
+    {
+        [SerializeField] private List<BuildingViewDescription> _descriptions;
+
+        private Dictionary<string, BuildingViewDescription> _dictionaryDescriptions;
+
+        public void Initialize()
+        {
+            _dictionaryDescriptions = new Dictionary<string, BuildingViewDescription>();
+            foreach (var description in _descriptions)
+            {
+                _dictionaryDescriptions.Add(description.BuildingDescriptionId, description);
+            }
+        }
+
+        public BuildingViewDescription GetById(string id)
+        {
+            return _dictionaryDescriptions[id];
+        }
+    }
+}
