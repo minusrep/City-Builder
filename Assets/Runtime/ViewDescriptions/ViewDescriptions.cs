6f2fc745897d64dc5df3044edeb1eb51dc5cc8d0
Valyutik <sergei.lind@tutamail.com>
2025-12-13 19:42:39 +0300

    add: view descriptions root

---
+++ Assets/Runtime/ViewDescriptions/ViewDescriptions.cs
@@ -0,0 +1,17 @@
+﻿using Runtime.ViewDescriptions.Buildings;
+using Runtime.ViewDescriptions.Inventory;
+
+namespace Runtime.ViewDescriptions
+{
+    public class ViewDescriptions
+    {
+        public BuildingViewDescriptionCollection BuildingViewDescriptions { get; }
+        public InventoryViewDescription InventoryViewDescription { get; }
+
+        public ViewDescriptions(BuildingViewDescriptionCollection buildingViewDescriptions, InventoryViewDescription inventoryViewDescription)
+        {
+            BuildingViewDescriptions = buildingViewDescriptions;
+            InventoryViewDescription = inventoryViewDescription;
+        }
+    }
+}
---
+++ Assets/Runtime/ViewDescriptions/ViewDescriptionsLoader.cs
@@ -0,0 +1,22 @@
+﻿using Runtime.ViewDescriptions.Buildings;
+using Runtime.ViewDescriptions.Inventory;
+using UnityEngine;
+
+namespace Runtime.ViewDescriptions
+{
+    public static class ViewDescriptionsLoader
+    {
+        public static ViewDescriptions Load()
+        {
+            var buildingViews =
+                Resources.Load<BuildingViewDescriptionCollection>(
+                    "ViewDescriptions/Buildings/BuildingViewDescriptionCollection");
+
+            var inventoryViews =
+                Resources.Load<InventoryViewDescription>(
+                    "ViewDescriptions/Items/InventoryViewDescription");
+
+            return new ViewDescriptions(buildingViews, inventoryViews);
+        }
+    }
+}
