1591a6af4abd47608e8fa5e29397fb49b18fd00e
Valyutik <sergei.lind@tutamail.com>
2025-12-15 21:41:15 +0300

    add: for other types of buildings description view

---
+++ Assets/Runtime/ViewDescriptions/Buildings/ServiceBuildingViewDescription.cs
@@ -0,0 +1,10 @@
+﻿using Runtime.Colony.Buildings.Service;
+using UnityEngine;
+
+namespace Runtime.ViewDescriptions.Buildings
+{
+    [CreateAssetMenu(fileName = "ServiceBuildingViewDescription", menuName = "Buildings/View/Service")]
+    public class ServiceBuildingViewDescription : BuildingViewDescription<ServiceBuildingView>
+    {
+    }
+}
---
+++ Assets/Runtime/ViewDescriptions/Buildings/StorageBuildingViewDescription.cs
@@ -0,0 +1,11 @@
+﻿using Runtime.Colony.Buildings.Storage;
+using UnityEngine;
+
+namespace Runtime.ViewDescriptions.Buildings
+{
+    [CreateAssetMenu(fileName = "StorageBuildingViewDescription", menuName = "Buildings/View/Storage")]
+    public class StorageBuildingViewDescription : BuildingViewDescription<StorageBuildingView>
+    {
+        
+    }
+}
