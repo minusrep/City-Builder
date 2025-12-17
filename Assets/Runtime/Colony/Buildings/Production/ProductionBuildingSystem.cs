43c732b811ede459d468078697b4974f9b0a7449
Valyutik <sergei.lind@tutamail.com>
2025-12-17 21:09:49 +0300

    refactor: move progress bar update to the system

--- Assets/Runtime/Colony/Buildings/Production/ProductionBuildingSystem.cs
+++ Assets/Runtime/Colony/Buildings/Production/ProductionBuildingSystem.cs
@@ -5,13 +5,13 @@ namespace Runtime.Colony.Buildings.Production
 {
     public class ProductionBuildingSystem : IGameSystem
     {
-        public event Action<long> OnUpdate;
-        
         private readonly ProductionBuildingModel _model;
+        private readonly ProductionBuildingView _view;
 
-        public ProductionBuildingSystem(ProductionBuildingModel model)
+        public ProductionBuildingSystem(ProductionBuildingModel model, ProductionBuildingView view)
         {
             _model = model;
+            _view = view;
         }
 
         public void Update(float deltaTime)
@@ -42,9 +42,15 @@ namespace Runtime.Colony.Buildings.Production
                         break;
                     }
                 }
-
-                OnUpdate?.Invoke(currentTime);
+                
+                UpdateProgressBar(currentTime);
             }
+        }
+
+        private void UpdateProgressBar(long currentTime)
+        {
+            var progress = (float)(currentTime - _model.StartProductionTime) / _model.Description.ProductionTime;
+            _view.ProgressBar.value = Math.Clamp(progress, 0f, 1f) * 100f;
         }
     }
 }
