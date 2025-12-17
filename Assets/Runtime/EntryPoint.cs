0fe3e28321df6212a275d1e9680e13e28194430f
DmitryDemenkov <demon.krut2002@mail.ru>
2025-12-17 22:12:45 +0300

    chg: entry point updated for loading through load steps

--- Assets/Runtime/EntryPoint.cs
+++ Assets/Runtime/EntryPoint.cs
@@ -1,18 +1,15 @@
 ﻿using System.Collections.Generic;
-using System.Threading.Tasks;
-using fastJSON;
 using Runtime.AsyncLoad;
 using Runtime.Colony;
 using Runtime.Colony.Buildings.Collection;
+using Runtime.Common;
 using Runtime.Colony.Buildings.Common.Factories;
 using Runtime.Colony.Citizens;
-using Runtime.Colony.Citizens.Systems;
 using Runtime.Colony.GameResources;
 using Runtime.Colony.StateMachine;
 using Runtime.Descriptions;
 using Runtime.GameSystems;
-using Runtime.ViewDescriptions.Buildings;
-using Runtime.ViewDescriptions.Inventory;
+using Runtime.Services.SaveLoadSteps;
 using UnityEngine;
 
 namespace Runtime
@@ -23,23 +20,17 @@ namespace Runtime
 
         [SerializeField] private CitizenView _citizenView;
         
-        private WorldDescription _worldDescription;
-        private ViewDescriptions.ViewDescriptions _viewDescriptions;
+        private readonly WorldDescription _worldDescription = new WorldDescription();
+        private readonly ViewDescriptions.ViewDescriptions _viewDescriptions  = new ViewDescriptions.ViewDescriptions();
 
-        private FactoryProvider _factoryProvider;
-
-        private ResourceFactory _resourceFactory;
-        private BuildingModelFactory _buildingModelFactory;
-
-        private World _world;
-
-        private BuildingCollectionPresenter _buildingCollectionPresenter;
+        private readonly World _world = new World();
         
-        private GameSystemCollection _gameSystemCollection;
+        private readonly GameSystemCollection _gameSystemCollection = new GameSystemCollection();
         
-        private AddressableModel _addressableModel;
+        private readonly AddressableModel _addressableModel = new AddressableModel();
+
         
-        private AddressablePresenter _addressablePresenter;
+        private readonly List<IPresenter> _presenters = new List<IPresenter>();
         
         private CitizenStatSystem _citizenFeedStatSystem;
         
@@ -53,40 +44,20 @@ namespace Runtime
 
         private async void Start()
         {
-            _gameSystemCollection = new GameSystemCollection();
-            
-            InitializeAddressable();
-
-            InitializeDescriptions();
-
-            await InitializeViewDescriptionsAsync();
-
-            InitializeModelFactories();
-            
-            _world = new World(_worldDescription, _factoryProvider, _gameSystemCollection);
-
-            _buildingCollectionPresenter = new BuildingCollectionPresenter(_world,
-                _buildingCollectionView, _worldDescription.BuildingCollection, _viewDescriptions, _gameSystemCollection);
-            _buildingCollectionPresenter.Enable();
-
-            _citizenModel = new CitizenModel(0, _world.WorldDescription.Citizens);
-
-            _citizenModel.Stats["satiety"] = 50;
-
-            _citizenPresenter = new CitizenPresenter(_citizenView, _citizenModel, _world);
-            
-            _stateMachineSystem = new StateMachineSystem(_citizenModel.StateMachine, _world, _citizenModel);
-            
-            _gameSystemCollection.Add(_stateMachineSystem);
-            
-            _world.Citizens.Add("0", _citizenModel);
-            
-            foreach (var citizen in _world.Citizens.Models.Values)
+            IStep[] loadSteps =
             {
-                _world.CitizenStarvationStatSystem.Register(citizen);
+                new AddressableLoadStep(_addressableModel, _presenters),
+                new DescriptionsLoadStep(_worldDescription),
+                new ViewDescriptionsLoadStep(_viewDescriptions, _addressableModel),
+                new WorldLoadStep(_world, _worldDescription),
+                new BuildingCollectionLoadStep(_presenters, _world, _buildingCollectionView, 
+                    _worldDescription, _viewDescriptions, _gameSystemCollection),
+            };
+
+            foreach (var step in loadSteps)
+            {
+                await step.Run();
             }
-            
-            _citizenPresenter.Enable();
         }
 
         private void Update()
@@ -96,57 +67,11 @@ namespace Runtime
 
         private void OnDisable()
         {
-            _buildingCollectionPresenter.Disable();
-        }
-
-        private void InitializeModelFactories()
-        {
-            _resourceFactory = new ResourceFactory(_worldDescription.ResourceCollection);
-            _buildingModelFactory = new BuildingModelFactory(_worldDescription);
-
-            _buildingModelFactory.RegisterAll();
-            _factoryProvider = new FactoryProvider(_resourceFactory, _buildingModelFactory);
-        }
-
-        private void InitializeDescriptions()
-        {
-            var buildingDescriptions =
-                JSON.ToObject<Dictionary<string, object>>(
-                    Resources.Load<TextAsset>("Descriptions/buildings_description").text);
-            var citizensDescriptions =
-                JSON.ToObject<Dictionary<string, object>>(
-                    Resources.Load<TextAsset>("Descriptions/citizens_description").text);
-            var resourcesDescriptions =
-                JSON.ToObject<Dictionary<string, object>>(
-                    Resources.Load<TextAsset>("Descriptions/items_description").text);
-
-            var data = new Dictionary<string, object>
+            _presenters.Reverse();
+            foreach (var presenter in _presenters)
             {
-                { "buildings", buildingDescriptions },
-                { "citizens", citizensDescriptions },
-                { "resources", resourcesDescriptions },
-            };
-
-            _worldDescription = new WorldDescription(data);
-        }
-
-        private async Task InitializeViewDescriptionsAsync()
-        {
-            var buildingViewLoad = _addressableModel.Load<BuildingViewDescriptionCollection>("BuildingViewDescriptionCollection");
-            var inventoryViewLoad = _addressableModel.Load<InventoryViewDescription>("InventoryViewDescription");
-
-            await buildingViewLoad.LoadAwaiter;
-            await inventoryViewLoad.LoadAwaiter;
-            
-            _viewDescriptions = new ViewDescriptions.ViewDescriptions(buildingViewLoad.Result, inventoryViewLoad.Result);
-        }
-
-        private void InitializeAddressable()
-        {
-            _addressableModel = new AddressableModel();
-            _addressablePresenter = new AddressablePresenter(_addressableModel);
-            
-            _addressablePresenter.Enable();
+                presenter.Disable();
+            }
         }
     }
 }
