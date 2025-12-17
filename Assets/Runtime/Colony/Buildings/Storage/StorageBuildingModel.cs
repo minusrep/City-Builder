49a8e1c0dc62fbd1685e894fdb1b4aae06abe040
Valyutik <sergei.lind@tutamail.com>
2025-12-18 01:26:18 +0300

    chg: use of inventory model in service building model

--- Assets/Runtime/Colony/Buildings/Storage/StorageBuildingModel.cs
+++ Assets/Runtime/Colony/Buildings/Storage/StorageBuildingModel.cs
@@ -1,9 +1,9 @@
 ﻿using System.Collections.Generic;
 using Runtime.Colony.Buildings.Common;
-using Runtime.Colony.GameResources;
 using Runtime.Colony.Inventory;
 using Runtime.Descriptions;
 using Runtime.Descriptions.Buildings;
+using Runtime.Descriptions.Items;
 using Runtime.Extensions;
 using UnityEngine;
 
@@ -11,73 +11,65 @@ namespace Runtime.Colony.Buildings.Storage
 {
     public class StorageBuildingModel : BuildingModel
     {
-        public InventoryModel Inventory { get; }
-        
-        private StorageBuildingDescription Description { get; }
-        private Dictionary<string, ResourceModel> _resources = new();
-
         private const int MaxStackSize = 20;
+
+        public InventoryModel Inventory { get; private set; }
+        private WorldDescription WorldDescription { get; }
+
+        private StorageBuildingDescription Description { get; }
 
         public StorageBuildingModel(string id,
             Vector2 position,
             StorageBuildingDescription description, WorldDescription worldDescription) : base(id, position, description)
         {
+            WorldDescription = worldDescription;
             Description = description;
 
-
             Inventory = new InventoryModel(description.StoredResources.Count, MaxStackSize, worldDescription.ResourceCollection);
+            
+            foreach (var resourceDescriptionId in description.StoredResources)
+            {
+                var resourceDescription = worldDescription.ResourceCollection.Descriptions[resourceDescriptionId];
+                Inventory.TryAddItem(resourceDescription, 0);    
+            }
         }
         
-        public bool TryAddResource(string resourceKey, ResourceModel resourceModel)
+        public bool TryAddResource(ResourceDescription resource, int amount)
         {
-            if (_resources.TryGetValue(resourceKey, out var stored))
-            {
-                if (stored.Amount + resourceModel.Amount <= Description.MaxResourceAmount)
-                {
-                    stored.IncreaseAmount(resourceModel.Amount);
-                    return true;
-                }
-            }
-
-            return false;
+            return Inventory.TryAddItem(resource, amount);
         }
         
-        public bool TryTakeResource(string resourceKey, int amount)
+        public bool TryTakeResource(ResourceDescription resource, int amount)
         {
-            if (_resources.TryGetValue(resourceKey, out var resource))
-            {
-                if (resource.Amount >= amount)
-                {
-                    resource.ReduceAmount(amount);
-                    return true;
-                }
-            }
-
-            return false;
+            return Inventory.TryRemoveItem(resource, amount);
         }
 
-        public int GetAmount(string resourceKey)
+        public int GetAmount(ResourceDescription resource)
         {
-            return _resources[resourceKey].Amount;
+            foreach (var cell in Inventory.Models.Values)
+            {
+                if (cell.Resource != null && cell.Resource == resource)
+                {
+                    return cell.Amount;
+                }
+            }
+            return 0;
         }
 
         public override Dictionary<string, object> Serialize()
         {
-            var dictionary = new Dictionary<string, object>(base.Serialize());
-            var serializedResources = new Dictionary<string, object>();
-            foreach (var kvp in _resources)
+            var dictionary = new Dictionary<string, object>(base.Serialize())
             {
-                serializedResources[kvp.Key] = kvp.Value.Serialize();
-            }
-
-            dictionary.Set("resources", serializedResources);
+                { "inventory", Inventory.Serialize() }
+            };
+            
             return dictionary;
         }
 
         public override void Deserialize(Dictionary<string, object> data)
         {
-            _resources = new Dictionary<string, ResourceModel>();
-            var node = data.GetNode("resources");
+            Inventory = new InventoryModel(1, MaxStackSize, WorldDescription.ResourceCollection);
+            Inventory.Deserialize(data.GetNode("inventory"));
         }
     }
 }
