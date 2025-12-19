bbe094be59c2aaf65a50ce301026919fc3aa1a4a
Valyutik <sergei.lind@tutamail.com>
2025-12-19 16:34:50 +0300

    refactor: remove view inheritance from monobehabior and pass a reference to the ui document

--- Assets/Runtime/Colony/Buildings/Storage/StorageBuildingView.cs
+++ Assets/Runtime/Colony/Buildings/Storage/StorageBuildingView.cs
@@ -1,9 +1,22 @@
 ﻿using Runtime.Colony.Buildings.Common;
+using UnityEngine;
+using UnityEngine.UIElements;
 
 namespace Runtime.Colony.Buildings.Storage
 {
     public class StorageBuildingView : BuildingView
     {
+        public UIDocument Document => _uiDocument;
+        public ProgressBar ProgressBar => _progressBar;
+
+        [SerializeField] private UIDocument _uiDocument;
+        private ProgressBar _progressBar;
         
+        public override void Initialize()
+        {
+            base.Initialize();
+            var root = _uiDocument.rootVisualElement;
+            _progressBar = root.Q<ProgressBar>("production-progress");
+        }
     }
 }
