b8bfe3b7eb6b97c90ceb6cf99e49c3fcb1eab96f
Maxim S. <its3zin.main@gmail.com>
2025-12-20 01:31:56 +0300

    add: game systems collection load step, citizen collection load step

---
+++ Assets/Runtime/Services/SaveLoadSteps/CitizenCollectionLoadStep.cs
@@ -0,0 +1,36 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Runtime.Colony;
+using Runtime.Colony.Citizens;
+using Runtime.Common;
+
+namespace Runtime.Services.SaveLoadSteps
+{
+    public class CitizenCollectionLoadStep : IStep
+    {
+        private readonly List<IPresenter> _presenters;
+
+        private readonly World _world;
+        
+        private readonly CitizenViewCollection _citizenViewCollection;
+
+        public CitizenCollectionLoadStep(List<IPresenter> presenters, World world, CitizenViewCollection citizenViewCollection)
+        {
+            _presenters = presenters;
+            _world = world;
+            _citizenViewCollection = citizenViewCollection;
+        }
+
+        public async Task Run()
+        {
+            var citizenCollectionPresenter =
+                new CitizenPresenterCollection(_citizenViewCollection, _world.Citizens, _world);
+            
+            citizenCollectionPresenter.Enable();
+            
+            _presenters.Add(citizenCollectionPresenter);
+            
+            await Task.CompletedTask;
+        }
+    }
+}
