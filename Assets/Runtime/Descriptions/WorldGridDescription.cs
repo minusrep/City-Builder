4cbdee8fc87747f36d8b598e353ff223073fef0d
Valyutik <sergei.lind@tutamail.com>
2026-01-05 17:12:06 +0300

    add: world grid description

---
+++ Assets/Runtime/Descriptions/WorldGridDescription.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using Runtime.Extensions;
+
+namespace Runtime.Descriptions
+{
+    public class WorldGridDescription
+    {
+        public int Width { get; }
+        public int Height { get; }
+        public float CellSize { get; }
+
+        public WorldGridDescription(Dictionary<string, object> data)
+        {
+            Width = data.GetInt("width");
+            Height = data.GetInt("height");
+            CellSize = data.GetFloat("cell_size");
+        }
+    }
+}
