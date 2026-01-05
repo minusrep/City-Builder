63f9de35ef7a9b948d70bfcbe9c899a33094f75f
Valyutik <sergei.lind@tutamail.com>
2026-01-05 20:18:31 +0300

    add: origin position

--- Assets/Runtime/Descriptions/WorldGridDescription.cs
+++ Assets/Runtime/Descriptions/WorldGridDescription.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using Runtime.Extensions;
+using UnityEngine;
 
 namespace Runtime.Descriptions
 {
@@ -8,12 +9,14 @@ namespace Runtime.Descriptions
         public int Width { get; }
         public int Height { get; }
         public float CellSize { get; }
+        public Vector2 Origin { get; }
 
         public WorldGridDescription(Dictionary<string, object> data)
         {
             Width = data.GetInt("width");
             Height = data.GetInt("height");
             CellSize = data.GetFloat("cell_size");
+            Origin = data.GetVector2("origin_position");
         }
     }
 }
