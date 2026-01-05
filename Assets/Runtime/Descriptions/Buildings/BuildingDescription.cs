00a5fc03098760af2322d4ec61fd328df5c1713c
Valyutik <sergei.lind@tutamail.com>
2026-01-05 15:29:47 +0300

    add: description of size and occupied cells in the building description

--- Assets/Runtime/Descriptions/Buildings/BuildingDescription.cs
+++ Assets/Runtime/Descriptions/Buildings/BuildingDescription.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using Runtime.Extensions;
+using UnityEngine;
 
 namespace Runtime.Descriptions.Buildings
 {
@@ -8,12 +9,16 @@ namespace Runtime.Descriptions.Buildings
         public string Id { get; }
         public string Type { get; }
         public string ViewDescriptionId { get; }
+        public Vector2Int Size { get; }
+        public IReadOnlyList<Vector2Int> Cells { get; }
 
         protected BuildingDescription(string id, Dictionary<string, object> data)
         {
             Id = id;
             Type = data.GetString("type");
             ViewDescriptionId = data.GetString("view_id");
+            Size = data.GetVector2Int("size");
+            Cells = data.GetList<Vector2Int>("cells");
         }
     }
 }
