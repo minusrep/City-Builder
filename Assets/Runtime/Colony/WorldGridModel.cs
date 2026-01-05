432df95bef2b39e77409e7baea30b3aa45e5f434
Valyutik <sergei.lind@tutamail.com>
2026-01-05 15:30:42 +0300

    add: cells and a world grid for buildings

---
+++ Assets/Runtime/Colony/WorldGridModel.cs
@@ -0,0 +1,26 @@
+﻿namespace Runtime.Colony
+{
+    public class WorldGridModel
+    {
+        public int Width { get; }
+        public int Height { get; }
+        
+        private GridCellModel[,] _cells;
+
+        public WorldGridModel(int width, int height, float cellSize)
+        {
+            Width = width;
+            Height = height;
+            
+            _cells = new GridCellModel[width, height];
+
+            for (var x = 0; x < Width; x++)
+            {
+                for (var y = 0; y < Height; y++)
+                {
+                    _cells[x, y] = new GridCellModel(x, y);
+                }
+            }
+        }
+    }
+}
