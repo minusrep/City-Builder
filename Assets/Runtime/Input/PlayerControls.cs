c6ea7614d73c2a2e7be7bf9ab813d2a418f7756e
Valyutik <sergei.lind@tutamail.com>
2026-01-05 21:34:40 +0300

    add: new action tracking cursor position for construction

--- Assets/Runtime/Input/PlayerControls.cs
+++ Assets/Runtime/Input/PlayerControls.cs
@@ -249,6 +249,34 @@ namespace Runtime.Input
                     ""isPartOfComposite"": false
                 }
             ]
+        },
+        {
+            ""name"": ""Construction"",
+            ""id"": ""2831b226-fe37-4170-8c49-1e29a97172b1"",
+            ""actions"": [
+                {
+                    ""name"": ""MovePreview"",
+                    ""type"": ""Value"",
+                    ""id"": ""46de0c11-6451-4e2d-88d0-edd7b964f4c3"",
+                    ""expectedControlType"": ""Vector2"",
+                    ""processors"": """",
+                    ""interactions"": """",
+                    ""initialStateCheck"": true
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": """",
+                    ""id"": ""ae8d87ea-d599-4f69-9256-be2146adaa56"",
+                    ""path"": ""<Pointer>/position"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""MovePreview"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                }
+            ]
         }
     ],
     ""controlSchemes"": []
@@ -262,12 +290,16 @@ namespace Runtime.Input
             // UI
             m_UI = asset.FindActionMap("UI", throwIfNotFound: true);
             m_UI_Pause = m_UI.FindAction("Pause", throwIfNotFound: true);
+            // Construction
+            m_Construction = asset.FindActionMap("Construction", throwIfNotFound: true);
+            m_Construction_MovePreview = m_Construction.FindAction("MovePreview", throwIfNotFound: true);
         }
 
         ~@PlayerControls()
         {
             UnityEngine.Debug.Assert(!m_Camera.enabled, "This will cause a leak and performance issues, PlayerControls.Camera.Disable() has not been called.");
             UnityEngine.Debug.Assert(!m_UI.enabled, "This will cause a leak and performance issues, PlayerControls.UI.Disable() has not been called.");
+            UnityEngine.Debug.Assert(!m_Construction.enabled, "This will cause a leak and performance issues, PlayerControls.Construction.Disable() has not been called.");
         }
 
         /// <summary>
@@ -564,6 +596,102 @@ namespace Runtime.Input
         /// Provides a new <see cref="UIActions" /> instance referencing this action map.
         /// </summary>
         public UIActions @UI => new UIActions(this);
+
+        // Construction
+        private readonly InputActionMap m_Construction;
+        private List<IConstructionActions> m_ConstructionActionsCallbackInterfaces = new List<IConstructionActions>();
+        private readonly InputAction m_Construction_MovePreview;
+        /// <summary>
+        /// Provides access to input actions defined in input action map "Construction".
+        /// </summary>
+        public struct ConstructionActions
+        {
+            private @PlayerControls m_Wrapper;
+
+            /// <summary>
+            /// Construct a new instance of the input action map wrapper class.
+            /// </summary>
+            public ConstructionActions(@PlayerControls wrapper) { m_Wrapper = wrapper; }
+            /// <summary>
+            /// Provides access to the underlying input action "Construction/MovePreview".
+            /// </summary>
+            public InputAction @MovePreview => m_Wrapper.m_Construction_MovePreview;
+            /// <summary>
+            /// Provides access to the underlying input action map instance.
+            /// </summary>
+            public InputActionMap Get() { return m_Wrapper.m_Construction; }
+            /// <inheritdoc cref="UnityEngine.InputSystem.InputActionMap.Enable()" />
+            public void Enable() { Get().Enable(); }
+            /// <inheritdoc cref="UnityEngine.InputSystem.InputActionMap.Disable()" />
+            public void Disable() { Get().Disable(); }
+            /// <inheritdoc cref="UnityEngine.InputSystem.InputActionMap.enabled" />
+            public bool enabled => Get().enabled;
+            /// <summary>
+            /// Implicitly converts an <see ref="ConstructionActions" /> to an <see ref="InputActionMap" /> instance.
+            /// </summary>
+            public static implicit operator InputActionMap(ConstructionActions set) { return set.Get(); }
+            /// <summary>
+            /// Adds <see cref="InputAction.started"/>, <see cref="InputAction.performed"/> and <see cref="InputAction.canceled"/> callbacks provided via <param cref="instance" /> on all input actions contained in this map.
+            /// </summary>
+            /// <param name="instance">Callback instance.</param>
+            /// <remarks>
+            /// If <paramref name="instance" /> is <c>null</c> or <paramref name="instance"/> have already been added this method does nothing.
+            /// </remarks>
+            /// <seealso cref="ConstructionActions" />
+            public void AddCallbacks(IConstructionActions instance)
+            {
+                if (instance == null || m_Wrapper.m_ConstructionActionsCallbackInterfaces.Contains(instance)) return;
+                m_Wrapper.m_ConstructionActionsCallbackInterfaces.Add(instance);
+                @MovePreview.started += instance.OnMovePreview;
+                @MovePreview.performed += instance.OnMovePreview;
+                @MovePreview.canceled += instance.OnMovePreview;
+            }
+
+            /// <summary>
+            /// Removes <see cref="InputAction.started"/>, <see cref="InputAction.performed"/> and <see cref="InputAction.canceled"/> callbacks provided via <param cref="instance" /> on all input actions contained in this map.
+            /// </summary>
+            /// <remarks>
+            /// Calling this method when <paramref name="instance" /> have not previously been registered has no side-effects.
+            /// </remarks>
+            /// <seealso cref="ConstructionActions" />
+            private void UnregisterCallbacks(IConstructionActions instance)
+            {
+                @MovePreview.started -= instance.OnMovePreview;
+                @MovePreview.performed -= instance.OnMovePreview;
+                @MovePreview.canceled -= instance.OnMovePreview;
+            }
+
+            /// <summary>
+            /// Unregisters <param cref="instance" /> and unregisters all input action callbacks via <see cref="ConstructionActions.UnregisterCallbacks(IConstructionActions)" />.
+            /// </summary>
+            /// <seealso cref="ConstructionActions.UnregisterCallbacks(IConstructionActions)" />
+            public void RemoveCallbacks(IConstructionActions instance)
+            {
+                if (m_Wrapper.m_ConstructionActionsCallbackInterfaces.Remove(instance))
+                    UnregisterCallbacks(instance);
+            }
+
+            /// <summary>
+            /// Replaces all existing callback instances and previously registered input action callbacks associated with them with callbacks provided via <param cref="instance" />.
+            /// </summary>
+            /// <remarks>
+            /// If <paramref name="instance" /> is <c>null</c>, calling this method will only unregister all existing callbacks but not register any new callbacks.
+            /// </remarks>
+            /// <seealso cref="ConstructionActions.AddCallbacks(IConstructionActions)" />
+            /// <seealso cref="ConstructionActions.RemoveCallbacks(IConstructionActions)" />
+            /// <seealso cref="ConstructionActions.UnregisterCallbacks(IConstructionActions)" />
+            public void SetCallbacks(IConstructionActions instance)
+            {
+                foreach (var item in m_Wrapper.m_ConstructionActionsCallbackInterfaces)
+                    UnregisterCallbacks(item);
+                m_Wrapper.m_ConstructionActionsCallbackInterfaces.Clear();
+                AddCallbacks(instance);
+            }
+        }
+        /// <summary>
+        /// Provides a new <see cref="ConstructionActions" /> instance referencing this action map.
+        /// </summary>
+        public ConstructionActions @Construction => new ConstructionActions(this);
         /// <summary>
         /// Interface to implement callback methods for all input action callbacks associated with input actions defined by "Camera" which allows adding and removing callbacks.
         /// </summary>
@@ -614,6 +742,21 @@ namespace Runtime.Input
             /// <seealso cref="UnityEngine.InputSystem.InputAction.performed" />
             /// <seealso cref="UnityEngine.InputSystem.InputAction.canceled" />
             void OnPause(InputAction.CallbackContext context);
+        }
+        /// <summary>
+        /// Interface to implement callback methods for all input action callbacks associated with input actions defined by "Construction" which allows adding and removing callbacks.
+        /// </summary>
+        /// <seealso cref="ConstructionActions.AddCallbacks(IConstructionActions)" />
+        /// <seealso cref="ConstructionActions.RemoveCallbacks(IConstructionActions)" />
+        public interface IConstructionActions
+        {
+            /// <summary>
+            /// Method invoked when associated input action "MovePreview" is either <see cref="UnityEngine.InputSystem.InputAction.started" />, <see cref="UnityEngine.InputSystem.InputAction.performed" /> or <see cref="UnityEngine.InputSystem.InputAction.canceled" />.
+            /// </summary>
+            /// <seealso cref="UnityEngine.InputSystem.InputAction.started" />
+            /// <seealso cref="UnityEngine.InputSystem.InputAction.performed" />
+            /// <seealso cref="UnityEngine.InputSystem.InputAction.canceled" />
+            void OnMovePreview(InputAction.CallbackContext context);
         }
     }
 }
