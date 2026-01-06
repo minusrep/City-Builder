a69bb01fdab655e2a1d502f6c7949d27138fac71
Valyutik <sergei.lind@tutamail.com>
2026-01-06 21:15:48 +0300

    add: visual size of the building in cells

--- Assets/Runtime/ViewDescriptions/Buildings/BuildingViewDescription.cs
+++ Assets/Runtime/ViewDescriptions/Buildings/BuildingViewDescription.cs
@@ -8,5 +8,7 @@ namespace Runtime.ViewDescriptions.Buildings
         public string Id => name;
         
         public BuildingView Prefab;
+        
+        public Vector2Int VisualSizeInCells = Vector2Int.one;
     }
 }
