2d43e7161f7330df597e8d24fed645ad05d38fa5
Valyutik <sergei.lind@tutamail.com>
2026-01-09 02:21:11 +0300

    add: change grid color

--- Assets/Runtime/Colony/Construction/WorldGridView.cs
+++ Assets/Runtime/Colony/Construction/WorldGridView.cs
@@ -7,13 +7,17 @@ namespace Runtime.Colony.Construction
     {
         public Transform Transform { get; private set; }
         public GameObject GameObject { get; private set; }
-        public MeshFilter Filter { get; private set; }
+        public MeshFilter Filter => _filter;
+
+        [SerializeField] private MeshFilter _filter;
+        [SerializeField] private Renderer _renderer;
+        [SerializeField] private Color _gridColor = Color.grey;
 
         private void Awake()
         {
             Transform = transform;
             GameObject = gameObject;
-            Filter = GetComponent<MeshFilter>();
+            _renderer.material.color = _gridColor;
         }
     }
 }
