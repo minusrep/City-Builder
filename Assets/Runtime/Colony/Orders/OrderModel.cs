c9506b5e20c42e68f95771c3405c52256f120861
DmitryDemenkov <demon.krut2002@mail.ru>
2026-01-08 22:56:30 +0300

    chg: orders structure

--- Assets/Runtime/Colony/Orders/OrderModel.cs
+++ Assets/Runtime/Colony/Orders/OrderModel.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections.Generic;
+using Runtime.Descriptions.Items;
 using Runtime.Extensions;
 using Runtime.ModelCollections;
 
@@ -6,10 +8,30 @@ namespace Runtime.Colony.Orders
 {
     public class OrderModel : ISerializeModel, IDeserializeModel
     {
-        public int Id { get; set; }
+        public Action<OrderModel> OnAmountChanged;
+        
+        public string Id { get; set; }
         public string FromBuildingId { get; }
 
-        public OrderModel(int id, string fromBuildingId)
+        public string Type { get; set; }
+        public string ResourceId { get; set; }
+
+        private int _amount;
+        public int Amount 
+        { 
+            get => _amount;
+            set
+            {
+                _amount = value;
+                OnAmountChanged?.Invoke(this);
+            }
+        }
+        
+        private int _selectedAmount;
+
+        public int FreeAmount => Amount - _selectedAmount;
+
+        public OrderModel(string id, string fromBuildingId)
         {
             Id = id;
             FromBuildingId = fromBuildingId;
@@ -17,12 +39,32 @@ namespace Runtime.Colony.Orders
 
         public Dictionary<string, object> Serialize() => new()
         {
-            { "id", Id }
+            { "id", Id },
+            { "type", Type },
+            { "resource", ResourceId },
+            { "amount", Amount },
+            { "selected_amount", _selectedAmount }
         };
 
         public void Deserialize(Dictionary<string, object> data)
         {
-            Id = data.GetInt("id");
+            Id = data.GetString("id");
+            Type = data.GetString("type");
+            ResourceId = data.GetString("resource");
+            Amount = data.GetInt("amount");
+            _selectedAmount = data.GetInt("selected_amount");
+        }
+
+        public void Select(int amount)
+        {
+            _selectedAmount += Math.Min(amount, FreeAmount) ;
+        }
+
+        public void Done(int amount)
+        {
+            amount = Math.Min(amount, _selectedAmount);
+            _selectedAmount -= amount;
+            Amount -= amount;
         }
     }
 }
--- Assets/Runtime/Colony/Orders/OrderModelCollection.cs
+++ Assets/Runtime/Colony/Orders/OrderModelCollection.cs
@@ -12,9 +12,21 @@ namespace Runtime.Colony.Orders
             _buildingId = buildingId;
         }
 
+        public override void Add(string key, OrderModel model)
+        {
+            if (Models.TryGetValue(key, out var order))
+            {
+                order.Amount += model.Amount;
+                return;
+            }
+            
+            base.Add(key, model);
+            model.OnAmountChanged += OnAmountChanged;
+        }
+
         protected override OrderModel CreateModelFromData(string id, Dictionary<string, object> data)
         {
-            var order = new OrderModel(Index, _buildingId);
+            var order = new OrderModel(id, _buildingId);
             order.Deserialize(data);
 
             return order;
@@ -22,8 +34,17 @@ namespace Runtime.Colony.Orders
 
         protected override OrderModel CreateModel()
         {
-            var order = new OrderModel(Index, _buildingId);
+            var order = new OrderModel(Index.ToString(), _buildingId);
             return order;
+        }
+        
+        private void OnAmountChanged(OrderModel model)
+        {
+            if (model.Amount <= 0)
+            {
+                model.OnAmountChanged -= OnAmountChanged;
+                Remove(model.Id);
+            }
         }
     }
 }
