3e51a3ef6cb6b5adf5b8cebf3288d5893f47c115
Alexei <153764172+MiramiKoi@users.noreply.github.com>
2026-01-09 19:22:14 +0300

    chg: refactor ResourceModel.cs

--- Assets/Runtime/Colony/GameResources/ResourceModel.cs
+++ Assets/Runtime/Colony/GameResources/ResourceModel.cs
@@ -17,23 +17,6 @@ namespace Runtime.Colony.GameResources
             Description = description;
         }
 
-        public void IncreaseAmount(int amount)
-        {
-            if (amount > 0)
-            {
-                Amount += amount;
-            }
-        }
-
-        public void ReduceAmount(int amount)
-        {
-            if (amount > 0)
-            {
-                var taken = Math.Min(Amount, amount);
-                Amount -= taken;
-            }
-        }
-
         public Dictionary<string, object> Serialize()
         {
             return new Dictionary<string, object>
