a615ec4ac751a1ba49702b4799e05f89efe91f04
Valyutik <sergei.lind@tutamail.com>
2026-01-15 02:45:25 +0300

    refactor: remove unnecessary links to player controls

--- Assets/Runtime/UI/HUD/HUDPresenter.cs
+++ Assets/Runtime/UI/HUD/HUDPresenter.cs
@@ -14,23 +14,20 @@ namespace Runtime.UI.HUD
 
         private readonly World _world;
 
-        private readonly PlayerControls _playerControls;
-
         private BuildingSelectionPresenter _buildingSelectionPresenter;
 
         private BuildingPanelPresenter _buildingPanelPresenter;
         
-        public HUDPresenter(HUDView view, HUDModel model, World world, PlayerControls playerControls)
+        public HUDPresenter(HUDView view, HUDModel model, World world)
         {
             _view = view;
             _model = model;
             _world = world;
-            _playerControls = playerControls;
         }
 
         public void Enable()
         {
-            _buildingSelectionPresenter = new BuildingSelectionPresenter(_view.BuildingSelectionView, _model.BuildingSelectionModel, _playerControls);
+            _buildingSelectionPresenter = new BuildingSelectionPresenter(_view.BuildingSelectionView, _model.BuildingSelectionModel, _world);
             
             _buildingPanelPresenter = new BuildingPanelPresenter(_view, _model.BuildingSelectionModel, _world);
             
