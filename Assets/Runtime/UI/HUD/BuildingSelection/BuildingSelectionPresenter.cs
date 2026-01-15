38dadd50f4a1643ae6fffab30a923b2d6a566c2a
Valyutik <sergei.lind@tutamail.com>
2026-01-15 03:52:51 +0300

    rmv: building selection view

--- Assets/Runtime/UI/HUD/BuildingSelection/BuildingSelectionPresenter.cs
+++ Assets/Runtime/UI/HUD/BuildingSelection/BuildingSelectionPresenter.cs
@@ -8,17 +8,15 @@ namespace Runtime.UI.HUD.BuildingSelection
 {
     public class BuildingSelectionPresenter : IPresenter
     {
-        private readonly BuildingSelectionView _view;
         private readonly BuildingSelectionModel _model;
         private readonly World _world;
 
         private BuildingView _cachedBuildingView;
         
-        public BuildingSelectionPresenter(BuildingSelectionView view, BuildingSelectionModel model, World world)
+        public BuildingSelectionPresenter(BuildingSelectionModel model, World world)
         {
             _model = model;
             _world = world;
-            _view = view;
         }
 
         public void Enable()
@@ -38,7 +36,7 @@ namespace Runtime.UI.HUD.BuildingSelection
                 return;
             }
             
-            var ray = _view.Camera.ScreenPointToRay(_world.PlayerControls.Player.PointerPosition.ReadValue<Vector2>());
+            var ray = _world.MainCamera.ScreenPointToRay(_world.PlayerControls.Player.PointerPosition.ReadValue<Vector2>());
             
             var success = Physics.Raycast(ray, out var hitInfo);
 
