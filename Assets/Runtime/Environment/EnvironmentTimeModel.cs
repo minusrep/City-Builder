f5836a0e6bd634050035fde3df04b3cdab45f085
Maxim S. <its3zin.main@gmail.com>
2026-01-17 02:26:15 +0300

    add: environment description

---
+++ Assets/Runtime/Environment/EnvironmentTimeModel.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using Runtime.Extensions;
+using Runtime.ModelCollections;
+
+namespace Runtime.Environment
+{
+    public class EnvironmentTimeModel : ISerializeModel, IDeserializeModel
+    {
+        private const string CurrentTimeKey = "current_time";
+
+        public event Action<float> OnTick;
+
+        public EnvironmentTimeDescription Description { get; private set; }
+        
+        private float CurrentTime { get; set; }
+
+        public EnvironmentTimeModel(EnvironmentTimeDescription description)
+        {
+            Description = description;
+        }
+
+        public void Tick(float deltaTime)
+        {
+            CurrentTime %=  Description.CycleLength;
+            
+            CurrentTime += deltaTime * Description.TimeScale;
+            
+            OnTick?.Invoke(CurrentTime);
+        }
+
+        public Dictionary<string, object> Serialize()
+        {
+            return new Dictionary<string, object>()
+            {
+                {CurrentTimeKey, CurrentTime}
+            };
+        }
+
+        public void Deserialize(Dictionary<string, object> data)
+        {
+            CurrentTime = data.GetFloat(CurrentTimeKey);
+        }
+    }
+}
