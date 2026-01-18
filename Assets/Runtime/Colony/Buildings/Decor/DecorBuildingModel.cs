37dbc029cf6bf7916071eb2f3c39b7710e05a7d1
Valyutik <sergei.lind@tutamail.com>
2026-01-19 02:50:07 +0300

    chg: implement circular interaction points for decor

--- Assets/Runtime/Colony/Buildings/Decor/DecorBuildingModel.cs
+++ Assets/Runtime/Colony/Buildings/Decor/DecorBuildingModel.cs
@@ -6,12 +6,21 @@ namespace Runtime.Colony.Buildings.Decor
 {
     public class DecorBuildingModel : BuildingModel
     {
+        private int _currentPointIndex;
+        
         public DecorBuildingModel(string id,
             Vector2Int gridPosition,
             DecorBuildingDescription baseDescription) : base(id,
             gridPosition,
             baseDescription)
         {
+        }
+
+        public override Vector3 GetInteractionPoint()
+        {
+            var point = WorldPosition + BaseDescription.InteractionPoints[_currentPointIndex];
+            _currentPointIndex = (_currentPointIndex + 1) % BaseDescription.InteractionPoints.Count;
+            return point;
         }
     }
 }
