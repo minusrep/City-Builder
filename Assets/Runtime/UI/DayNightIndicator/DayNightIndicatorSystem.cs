d753dea2706f5d6137b42264bbc2d8ec233fb064
Valyutik <sergei.lind@tutamail.com>
2026-01-20 02:50:04 +0300

    refactor: smooth day-night indicator rotation

--- Assets/Runtime/UI/DayNightIndicator/DayNightIndicatorSystem.cs
+++ Assets/Runtime/UI/DayNightIndicator/DayNightIndicatorSystem.cs
@@ -1,15 +1,19 @@
 ﻿using Runtime.Colony;
 using Runtime.GameSystems;
+using UnityEngine;
 using UnityEngine.UIElements;
 
 namespace Runtime.UI.DayNightIndicator
 {
     public class DayNightIndicatorSystem : IGameSystem
     {
+        private const float RotationOffset = 90f;
         public string Id => "day_night_indicator";
 
         private readonly DayNightIndicatorView _view;
         private readonly World _world;
+        
+        private float _currentAngle;
 
         public DayNightIndicatorSystem(DayNightIndicatorView view, World world)
         {
@@ -22,8 +26,10 @@ namespace Runtime.UI.DayNightIndicator
             var currentTime = _world.Environment.Time.CurrentTime;
             var cycleLength = _world.Environment.Description.Time.CycleLength;
             var normalizedTime = currentTime / cycleLength;
-            var angle = normalizedTime * 360f;
-            _view.IndicatorInner.style.rotate = new StyleRotate(new Angle(angle, AngleUnit.Degree));
+            var targetAngle = normalizedTime * 360f + RotationOffset;
+
+            _currentAngle = Mathf.LerpAngle(_currentAngle, targetAngle, deltaTime);
+            _view.IndicatorInner.style.rotate = new StyleRotate(new Angle(_currentAngle, AngleUnit.Degree));
         }
     }
 }
