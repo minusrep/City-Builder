200cc07949f5d4e4c3b85b0ad1cce7dec1e99298
DmitryDemenkov <demon.krut2002@mail.ru>
2026-01-19 17:11:08 +0300

    fix: negative values in cells

--- Assets/Runtime/Colony/Inventory/Cell/CellPresenter.cs
+++ Assets/Runtime/Colony/Inventory/Cell/CellPresenter.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine.UIElements;
 
 namespace Runtime.Colony.Inventory.Cell
@@ -27,11 +28,11 @@ namespace Runtime.Colony.Inventory.Cell
 
         private void UpdateView()
         {
-            _view.Amount.text = _model.Amount > 0 ? _model.Amount.ToString() : "";
+            _view.Amount.text = Math.Max(_model.Amount, 0).ToString();
             
             _view.Amount.style.display = DisplayStyle.Flex;
 
-            if (_model.Resource == null || _model.Amount <= 0)
+            if (_model.Resource == null || _model.Amount < 0)
             {
                 _view.Image.style.backgroundImage = null;
                 return;
