cca05f1c13228a52ba3a0785117f827b17c33334
Valyutik <sergei.lind@tutamail.com>
2026-01-20 01:02:27 +0300

    add: day-night indicator presenter and system

---
+++ Assets/Runtime/UI/DayNightIndicator/DayNightIndicatorPresenter.cs
@@ -0,0 +1,33 @@
+﻿using Runtime.Colony;
+using Runtime.Common;
+
+namespace Runtime.UI.DayNightIndicator
+{
+    public class DayNightIndicatorPresenter : IPresenter
+    {
+        private readonly DayNightIndicatorView _view;
+        private readonly World _world;
+        private readonly MenuContent _menuContent;
+        private readonly DayNightIndicatorSystem _system;
+
+        public DayNightIndicatorPresenter(DayNightIndicatorView view, World world, MenuContent menuContent)
+        {
+            _view = view;
+            _world = world;
+            _menuContent = menuContent;
+            _system = new DayNightIndicatorSystem(view, world);
+        }
+
+        public void Enable()
+        {
+            _menuContent.HudLayer.Add(_view.Root);
+            _world.GameSystems.Add(_system);
+        }
+
+        public void Disable()
+        {
+            _world.GameSystems.Remove(_system);
+            _menuContent.HudLayer.Remove(_view.Root);
+        }
+    }
+}
---
+++ Assets/Runtime/UI/DayNightIndicator/DayNightIndicatorSystem.cs
@@ -0,0 +1,29 @@
+﻿using Runtime.Colony;
+using Runtime.GameSystems;
+using UnityEngine.UIElements;
+
+namespace Runtime.UI.DayNightIndicator
+{
+    public class DayNightIndicatorSystem : IGameSystem
+    {
+        public string Id => "day_night_indicator";
+
+        private readonly DayNightIndicatorView _view;
+        private readonly World _world;
+
+        public DayNightIndicatorSystem(DayNightIndicatorView view, World world)
+        {
+            _view = view;
+            _world = world;
+        }
+
+        public void Update(float deltaTime)
+        {
+            var currentTime = _world.Environment.Time.CurrentTime;
+            var cycleLength = _world.Environment.Description.Time.CycleLength;
+            var normalizedTime = currentTime / cycleLength;
+            var angle = normalizedTime * 360f;
+            _view.IndicatorInner.style.rotate = new StyleRotate(new Angle(angle, AngleUnit.Degree));
+        }
+    }
+}
